c25530c27f8498289990c7eed190e463c14d9982
Hao Wu <hao.wu@argusmedia.com>
2025-01-14 18:54:48 +0000

    update example

--- inst/app/app.R
+++ inst/app/app.R
@@ -2,7 +2,9 @@ library(shiny)
 library(shiny.shadcn)
 
 tag_list <- shiny::tagList(
+  h2("Action button", style = "margin:1rem"),
   action_button("btn", "Click me"),
+  h2("Checkbox input", style = "margin:1rem"),
   shiny.shadcn::checkboxInput("checkbox", label = "Accept terms and conditions",
                               description = "Please read the terms and conditions before accepting")
 )
